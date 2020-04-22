28e00a56f226c65da4d0f6c08e678d1cf72537b9
lian.jing <lian.jing@ominidata.com>
2020-04-22 13:00:56 +0800

    feat(*):搭建框架

---
+++ src/views/Home.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="home">
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="500px"
+      title="添加链接"
+      @open="handleOpen"
+    >
+      <el-form
+        label-width="90px"
+        label-position="right"
+        size="small"
+        :model="formModel"
+        ref="form"
+        :rules="rules"
+      >
+        <el-form-item label="链接文本 ">
+          <el-input v-model="formModel.text" class="input-with-select">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="链接地址" prop="href">
+          <el-input v-model="formModel.href" class="input-with-select">
+            <el-select
+              v-model="formModel.protocol"
+              slot="prepend"
+              placeholder="请选择"
+            >
+              <el-option label="http://" value="http://"></el-option>
+              <el-option label="https://" value="https://"></el-option>
+            </el-select>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="指定浏览器">
+          <el-radio-group v-model="formModel.app">
+            <el-radio-button label="iexplore">IE</el-radio-button>
+            <el-radio-button label="chrome">Chrome</el-radio-button>
+            <el-radio-button label="firefox">Firefox</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false" size="small">取 消</el-button>
+        <el-button type="primary" @click="handleSubmit" size="small"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+    <el-button
+      size="mini"
+      v-for="(link, index) in linkList"
+      :key="index"
+      @click="openLink(link)"
+    >
+      {{ link.text }}
+    </el-button>
+    <el-button @click="dialogVisible = true">添加链接</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import open from 'open'
+
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      linkList: [],
+      formModel: {
+        href: '',
+        text: '',
+        app: 'iexplore',
+        protocol: 'https://',
+      },
+      rules: {
+        href: [{ required: true, message: '请输入链接地址', trigger: 'blur' }],
+      },
+
+      dialogVisible: false,
+    }
+  },
+  methods: {
+    openLink(link) {
+      open(link.protocol + link.href, { app: link.app || 'iexplore' })
+    },
+    handleOpen() {
+      this.$nextTick(() => {
+        this.$refs.form.resetFields()
+        this.formModel.app = 'iexplore'
+        this.formModel.protocol = 'https://'
+      })
+    },
+    handleSubmit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.linkList.push({ ...this.formModel })
+          this.dialogVisible = false
+        }
+      })
+    },
+  },
+}
+</script>
+<style>
+.el-select .el-input {
+  width: 90px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
+</style>
