0bd63bd075c79e79c06a89b75544c1567d98b348
lian.jing <lian.jing@shuyun.com>
2020-05-06 13:22:02 +0800

    v0.1.0 test again

--- src/updater.js
+++ src/updater.js
@@ -21,22 +21,21 @@ autoUpdater.on('error', error => {
 })
 
 autoUpdater.on('update-available', () => {
-  dialog.showMessageBox(
-    {
+  dialog
+    .showMessageBox({
       type: 'info',
       title: '软件有更新',
       message: '检查到软件有最新版本，是否立即更新',
       buttons: ['更新', '不更新'],
-    },
-    buttonIndex => {
+    })
+    .then(({ response: buttonIndex }) => {
       if (buttonIndex === 0) {
         autoUpdater.downloadUpdate()
       } else {
         updater.enabled = true
         updater = null
       }
-    }
-  )
+    })
 })
 
 autoUpdater.on('update-not-available', () => {
@@ -49,15 +48,14 @@ autoUpdater.on('update-not-available', () => {
 })
 
 autoUpdater.on('update-downloaded', () => {
-  dialog.showMessageBox(
-    {
+  dialog
+    .showMessageBox({
       title: '安装更新',
       message: '更新已下载，重启更新',
-    },
-    () => {
+    })
+    .then(() => {
       setImmediate(() => autoUpdater.quitAndInstall())
-    }
-  )
+    })
 })
 
 // export this to MenuItem click callback
